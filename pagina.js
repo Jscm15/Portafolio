1ecead8d9239ec394a93dc3383d0d4b8b3d67203
Jscm15 <jscmsebastian@gmail.com>
2022-12-01 09:56:21 -0500

    Portafolio

---
+++ pagina.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const User= require('../user')
+const service= require ('../services')
+
+function showGithub(){
+    window.location.href="https://github.com/Jscm15/Calculadora.git"
+}
+
+function Entregable(){
+    window.location.href="https://github.com/Jscm15/Entregable-Frontend-2.git"
+}
+
+function LoginCra(){
+window.location.href="https://github.com/Jscm15/Login-en-React.git"
+}
+
+function clinicaDental(){
+window.location.href="https://github.com/Jscm15/Clinica-Dental-Back.git"
+}
+
+function boton(){
+    window.location.href="#inicio "
+}
